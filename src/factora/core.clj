97388b548e1f9c6da8f2757e047722d1fc58502a
Eduardo Raad <eduardo.raad@datilmedia.com>
2014-09-12 02:12:25 -0500

    Docstr en API público.

--- src/factora/core.clj
+++ src/factora/core.clj
@@ -1,10 +1,11 @@
 (ns factora.core
+  "API público para emisión de comprobantes electrónicos."
   (:require [clojure.data.xml :as xml]))
 
 ;; Testing
 ;; http://stackoverflow.com/questions/17138518/convert-hyphenated-string-to-camelcase
 (defn hyphenated-name-to-camel-case-name [^String method-name]
-  (clojure.string/replace method-name #"-(\w)" 
+  (clojure.string/replace method-name #"-(\w)"
                           #(clojure.string/upper-case
                             (do
                               (println %1)
